c675a576ee08ba2819b851acaab40067a77b239c
Rim-bit <95483567+Rim-bit@users.noreply.github.com>
2026-06-14 01:45:00 -0400

    Add app config and SQLAlchemy

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from config import Config
 
+
+db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
+    app.config.from_object(Config)
+    db.init_app(app)
 
     # Home page
     @app.route('/')
