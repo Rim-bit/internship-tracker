73ca39caa896fc718e6c4a897edaf55cc79cd680
Rim-bit <95483567+Rim-bit@users.noreply.github.com>
2026-06-23 15:28:53 -0400

    Save, read, and display user inputs

--- app/routes.py
+++ app/routes.py
@@ -9,18 +9,27 @@ main_bp = Blueprint("main", __name__)
 @main_bp.route('/', methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
-        company_name = request.form['company']
-        company_data = Application(company=company_name)
-
+        new_application = Application(
+            company = request.form['company'],
+            role = request.form['role'],
+            status = request.form['status'],
+            location = request.form['location'],
+            applied_date = request.form['applied_date'],
+            deadline = request.form['deadline'],
+            link = request.form['link']
+        )
         try:
-            db.session.add(company_data)
+            db.session.add(new_application)
             db.session.commit()
             return redirect(url_for("main.index"))
         except:
             return "There was an issue adding company name"
     else:
-        return render_template('index.html')
+        internship_info = Application.query.order_by(Application.id).all()
+        return render_template('index.html', internship_info=internship_info)
 
 @main_bp.route('/about')
 def about():
     return "About page!"
+
+
