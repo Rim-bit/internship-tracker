bc3fa54a7a36b659be4f1b2074bafa8fca4b6909
Rim-bit <95483567+Rim-bit@users.noreply.github.com>
2026-06-22 21:05:52 -0400

    Add more columns

--- app/models.py
+++ app/models.py
@@ -1,12 +1,18 @@
 from app import db
 from datetime import datetime, UTC
-# model = blueprint for database
+
 class Application(db.Model):
     # primary_key means each row gets unique identifier
     id = db.Column(db.Integer, primary_key=True)
     company = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(200), nullable=True)
     status = db.Column(db.String(200), nullable=True)
+    location = db.Column(db.String(200), nullable=True)
+    applied_date = db.Column(db.String(200), nullable=True)
+    deadline = db.Column(db.String(200), nullable=True)
+    link = db.Column(db.String(200), nullable=True)
 
     # Return a readable string representation of one Application row
     def __repr__(self):
         return '<Application %r>' % self.id
+  
